96834105b48e35f6299d68be10ebbf806990631d
yeunglele <yangle@cecchina.com>
2025-06-15 09:31:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,6 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
